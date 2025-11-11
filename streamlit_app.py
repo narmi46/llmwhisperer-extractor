1e95ea803f3ef785f440794dbc61eb2ad10747d7
narmi46 <imran.salam46@gmail.com>
2025-11-11 15:24:31 +0800

    Fix: use official llmwhisperer-client package

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 from dotenv import load_dotenv
 
 # Your client comes from this package
-from unstract.llmwhisperer import LLMWhispererClientV2
+from llmwhisperer.client import LLMWhispererClientV2
 
 DEFAULT_BASE_URL = "https://llmwhisperer-api.us-central.unstract.com/api/v2"
 
